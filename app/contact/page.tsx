c695c911841627526cde40fe5f620a780ff5261c
nofurtherinformation <dhalpern@uchicago.edu>
2023-10-06 12:28:21 -0500

    static pages

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div className="prose max-w-3xl mx-auto py-4">
+      <h1>Contact</h1>
+      <p>
+        Reach out at{" "}
+        <a href="mailto:info@inclusivedevelopment.net">
+          info@inclusivedevelopment.net
+        </a>
+      </p>
+    </div>
+  );
+}
