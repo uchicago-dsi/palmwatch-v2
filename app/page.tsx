c6116de62294b7cfd052f18c6739cf8767c90180
nofurtherinformation <dhalpern@uchicago.edu>
2023-10-10 06:48:55 -0500

    Remove text

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ export default async function Home() {
         </a>
       </section>
 
-      <section className="max-w-none w-full  flex flex-col space-y-4 bg-neutral-200 mb-4 prose justify-center items-center lg:h-[100vh] lg:flex-row lg:space-x-4 lg:space-y-0">
+      {/* <section className="max-w-none w-full  flex flex-col space-y-4 bg-neutral-200 mb-4 prose justify-center items-center lg:h-[100vh] lg:flex-row lg:space-x-4 lg:space-y-0">
         <div className="flex w-full m-4 lg:m-0 lg:max-w-[25%] p-4 h-[50vh] rounded-xl shadow-xl bg-neutral-100">
           <h3>How is palm oil produced?</h3>
         </div>
@@ -79,7 +79,7 @@ export default async function Home() {
         <div className="flex w-full m-4 lg:m-0 lg:max-w-[25%] p-4 h-[50vh] rounded-xl shadow-xl bg-neutral-100">
           <h3>What can this site help me learn?</h3>
         </div>
-      </section>
+      </section> */}
       <section
         id="homepage-map"
         className="bg-white/30 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg mx-auto w-[90%] relative block"
@@ -87,7 +87,7 @@ export default async function Home() {
         <h3 className="text-xl my-4 font-bold">
           Palm Oil Mill Deforestation Map
         </h3>
-        <p>TODO: Map Explanation</p>
+        {/* <p>TODO: Map Explanation</p> */}
         <QueryProvider>
           <div className="h-[80vh] relative w-full">
             <PalmwatchMap
