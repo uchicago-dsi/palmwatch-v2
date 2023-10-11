2fd9d80f716da010e7826360565b0b4ad86801ff
nofurtherinformation <dhalpern@uchicago.edu>
2023-10-11 09:34:32 -0500

    Feedback button fix

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -3,28 +3,30 @@ import React from "react";
 import { usePathname } from "next/navigation";
 export const Feedback = () => {
   const path = usePathname();
-  const modal = React.useRef(null);
+  const url = path === "/" ? "Homepage" : decodeURI(path);
   const [showTooltip, setShowTooltip] = React.useState(false);
-  const baseUrl = typeof window !== "undefined" ? window.location.origin : "";
-  const currUrl = `${baseUrl}${path}`;
+  const [modalOpen, setModalOpen] = React.useState(false);
 
   const copyCurrUrlToClipboard = () => {
-    navigator.clipboard.writeText(currUrl);
+    navigator.clipboard.writeText(url);
     setShowTooltip(true);
     setTimeout(() => {
       setShowTooltip(false);
     }, 5000);
   };
+  const handleOpen = () => {
+    setModalOpen(true);
+  }
   return (
     <>
       <button
-        className="btn btn-sm normal-case rounded-r-none fixed right-0 bottom-[50%] translate-y-[50%] bg-accent rounded-full shadow-xl z-50"
+        className="btn btn-sm normal-case rounded-r-none fixed right-0 bottom-[50%] bg-accent rounded-full shadow-xl z-50"
         // @ts-ignore
-        onClick={() => modal?.current?.showModal()}
+        onClick={handleOpen}
       >
         Feedback
       </button>
-      <dialog className="modal" ref={modal}>
+      <dialog className={`modal ${modalOpen ? 'modal-open' : ''} `}>
         <div className="modal-box prose flex flex-col h-full">
           <h3 className="font-bold text-lg flex-0">PalmWatch Feedback Form</h3>
           <p className="flex-0">
@@ -34,7 +36,8 @@ export const Feedback = () => {
           </p>
           {/* add copy paste functionality */}
           <div className="flex-0 flex flex-row space-x-4 justify-center items-center">
-            <pre className="flex-1">{currUrl}</pre>
+            <p>PalmWatch website URL: </p>
+            <pre className="flex-1">{url}</pre>
             <div
               className={`flex-0 tooltip ${
                 showTooltip ? "tooltip-open" : "disabled"
@@ -48,17 +51,21 @@ export const Feedback = () => {
           </div>
           <iframe
             src="https://docs.google.com/forms/d/e/1FAIpQLSc_bWuT5T4WKu0kfU4rbuUqaSlbCtNfTTWdRrt2pc7AmShqUQ/viewform?embedded=true"
-            className="flex-1"
+            className="flex-1 shadow-inner border-neutral-500"
           >
             Loading…
           </iframe>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
+              <button className="btn" onClick={() => setModalOpen(false)}>Close</button>
             </form>
+
           </div>
         </div>
+            <form method="dialog" className="modal-backdrop">
+              <button onClick={() => setModalOpen(false)}>close</button>
+            </form>
       </dialog>
     </>
   );
