cd249619e350b2303728585131ebfa744b274edd
nofurtherinformation <dhalpern@uchicago.edu>
2024-01-09 13:04:46 -0600

    Vercel analytics

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 import queryClient from "@/utils/getMillData";
 import "./globals.css";
 import { NavBar } from "@/components/NavBar";
-import { promises as fs } from 'fs';
-import path from 'path';
+import { promises as fs } from "fs";
+import path from "path";
 import { Feedback } from "@/components/Feedback";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "PalmWatch",
@@ -24,15 +25,16 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const dataDir = path.join(process.cwd(), 'public', 'data');
+  const dataDir = path.join(process.cwd(), "public", "data");
   await queryClient.init(dataDir);
   const searchList = queryClient.getSearchList();
-  
+
   return (
     <html lang="en" data-theme="lemonade">
       <body className={inter.variable}>
         <NavBar searchList={searchList} currentPage="">
-        {children}
+          {children}
+          <Analytics />
         </NavBar>
         <Feedback />
       </body>
