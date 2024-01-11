f7f3bcec333a96cdf96230432cbfb38dbcef929c
nofurtherinformation <dhalpern@uchicago.edu>
2024-01-11 16:50:44 -0600

    Update schema.ts

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,9 +1,11 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import brandInfo from './schemas/brandInfo'
+import millInfo from './schemas/millInfo'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    brandInfo
+    brandInfo,
+    millInfo
   ],
 }
 
