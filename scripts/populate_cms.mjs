dee91134feda341b92269dbff5e375c5847bfb77
nofurtherinformation <dhalpern@uchicago.edu>
2024-01-23 10:54:51 -0600

    sanity population template

---
+++ scripts/populate_cms.mjs
@@ -0,0 +1,61 @@
+#!/bin/bash/zx
+// read cms_config.json
+// exported from nav bar search
+const config = await JSON.parse(await $`cat scripts/cms_config.json`)
+const sanityToken = '' // token
+const sanityId = '' // id
+const datasetName = 'production'
+
+const crudConfig = {
+  makeBrands: false,
+  makeMills: false,
+  makeCountries: true,
+  makeSuppliers: true
+}
+
+const batchSize = 25
+
+const makeMutations = (elements, type, prop) => {
+  return {
+    mutations: elements.map((element) => ({
+      createOrReplace: {
+        _type: type,
+        name: element[prop],
+        id: element[prop],
+      },
+    })),
+  }
+}
+
+const makeAndDoMutations = async (elements, type, prop) => {
+  const mutationCalls = []
+  for (let i=0;i<elements.length;i+=batchSize) {
+    mutationCalls.push(makeMutations(elements.slice(i, i+batchSize), type, prop))
+  }
+  // call each mutation
+  for (const mutations of mutationCalls) {
+    await $`curl 'https://${sanityId}.api.sanity.io/v2021-06-07/data/mutate/${datasetName}' \
+    -H 'Authorization: Bearer ${sanityToken}' \
+    -H 'Content-Type: application/json' \
+    --data-binary ${JSON.stringify(mutations)}`
+  }
+}
+
+if (crudConfig.makeBrands) {
+  await makeAndDoMutations(config.brands, 'brand', 'brand')
+}
+
+if (crudConfig.makeMills) {
+  const mills = config.Mills.map(mill => ({
+    mill: mill.href.replace("/mill/", "")
+  }))
+  await makeAndDoMutations(mills, 'mill', 'mill')
+}
+
+if (crudConfig.makeCountries) {
+  await makeAndDoMutations(config.Countries, 'country', 'label')
+}
+
+if (crudConfig.makeSuppliers) {
+  await makeAndDoMutations(config.Suppliers, 'supplier', 'label')
+}
