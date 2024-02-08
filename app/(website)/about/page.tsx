6c47c4d2ec2f73104700c3aa2b9e9bdc8d30ab2a
nofurtherinformation <dhalpern@uchicago.edu>
2024-02-08 08:28:02 -0600

    Hide FAQ title

--- app/(website)/about/page.tsx
+++ app/(website)/about/page.tsx
@@ -7,18 +7,22 @@ export default async function Page() {
     <div className="prose max-w-3xl mx-auto py-4">
       <PortableText value={aboutPageContent.content} />
       <div>
-        <h3>FAQ</h3>
-        {!!aboutPageContent?.faq?.length && aboutPageContent?.faq?.map((item, index) => (
-          <div key={index} className="collapse bg-base-200 my-4 shadow-xl">
-            <input type="radio" name={`faq-accordion`} />
-            <div className="collapse-title text-xl font-medium">
-              {item.heading}
-            </div>
-            <div className="collapse-content">
-              <PortableText value={item.body} />
-            </div>
-          </div>
-        ))}
+        {!!aboutPageContent?.faq?.length && (
+          <>
+            <h3>FAQ</h3>
+            {aboutPageContent.faq.map((item, index) => (
+              <div key={index} className="collapse bg-base-200 my-4 shadow-xl">
+                <input type="radio" name={`faq-accordion`} />
+                <div className="collapse-title text-xl font-medium">
+                  {item.heading}
+                </div>
+                <div className="collapse-content">
+                  <PortableText value={item.body} />
+                </div>
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
