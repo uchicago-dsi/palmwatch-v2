d2dbdc8d8989d6fc1ab7ca54803d85046b430bb7
nofurtherinformation <dhalpern@uchicago.edu>
2024-02-09 11:48:21 -0600

    Fix about page typo ...

--- sanity/schemas/aboutPage.ts
+++ sanity/schemas/aboutPage.ts
@@ -11,7 +11,7 @@ export default {
       type: "string",
     },
     {
-      ...getGenericContentConfig("conrtent"),
+      ...getGenericContentConfig("content"),
       title: "Main Content",
     },
     {
