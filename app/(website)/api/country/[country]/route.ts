638f25335922c2e065b4d573f8ae9db83f89442e
nofurtherinformation <dhalpern@uchicago.edu>
2024-02-20 14:20:41 -0600

    country api fix

--- app/(website)/api/country/[country]/route.ts
+++ app/(website)/api/country/[country]/route.ts
@@ -3,15 +3,15 @@ import queryClient from "@/utils/getMillData";
 import { NextResponse } from "next/server";
 import path from "path";
 
-export async function GET(_req: Request, res: { params: { group: string } }) {
-  const { group } = res.params;
-  if (!group)
+export async function GET(_req: Request, res: { params: { country: string } }) {
+  const { country } = res.params;
+  if (!country)
     return NextResponse.json(
       { error: new Error("No brand provided") },
       { status: 400 }
     );
   const dataDir = path.join(process.cwd(), "public", "data");
   await queryClient.init(dataDir);
-  const data = queryClient.getGroupInfo(group, fullYearRangeColumns);
+  const data = queryClient.getCountryData(country);
   return NextResponse.json({ ...data }, { status: 200 });
 }
