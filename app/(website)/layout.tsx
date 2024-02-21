1417ae4c494ba746a2c7c94dac2d4f9889609785
nofurtherinformation <dhalpern@uchicago.edu>
2024-02-21 12:04:27 -0600

    use vercel domain env variable

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -7,6 +7,10 @@ import cmsClient from "@/sanity/lib/client";
 import Head from "next/head";
 import { Footer } from "@/components/Footer";
 
+const VERCEL_DOMAIN = `${
+  process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
+}`;
+
 export const metadata = {
   title: "PalmWatch: Tracking the impact of Big Brands' Palm Oil Use",
   description:
@@ -14,14 +18,14 @@ export const metadata = {
   keywords:
     "PalmWatch, palm oil, deforestation, environmental impact, data science, open-source intelligence, global supply chain, Nestlé, PepsiCo, Unilever",
   robots: "index, follow",
-  metadataBase: new URL("https://www.palmwatch.inclusivedevelopment.net/"),
+  metadataBase: new URL(`${VERCEL_DOMAIN}/`),
   openGraph: {
-    url: "https://www.palmwatch.inclusivedevelopment.net/",
+    url: `${VERCEL_DOMAIN}`,
     title: "PalmWatch:  Tracking the impact of Big Brands' Palm Oil Use",
     description:
       "Discover how major brands' palm oil use drives deforestation and environmental change with PalmWatch, an open-access tool powered by data science and open-source intelligence.",
     images: {
-      url: "https://www.palmwatch.inclusivedevelopment.net/og-image.png",
+      url: `${VERCEL_DOMAIN}/og-image.png`,
       width: "1200",
       height: "630",
       alt: "PalmWatch: Tracking the impact of Big Brands' Palm Oil Use",
@@ -33,7 +37,7 @@ export const metadata = {
     description:
       "PalmWatch reveals the environmental footprint of global palm oil consumption by brands like Nestlé, PepsiCo, and Unilever through advanced data analysis.",
     images: {
-      url: "https://www.palmwatch.inclusivedevelopment.net/og-image.png",
+      url: `${VERCEL_DOMAIN}/og-image.png`,
       width: "1200",
       height: "630",
       alt: "PalmWatch: Tracking the impact of Big Brands' Palm Oil Use",
