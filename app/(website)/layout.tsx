f2104f29cfe9b6ac0e574826560301a8192ee11e
nofurtherinformation <dhalpern@uchicago.edu>
2024-02-22 14:09:34 -0600

    Meta capitalization fix

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -8,11 +8,11 @@ import Head from "next/head";
 import { Footer } from "@/components/Footer";
 
 const VERCEL_DOMAIN = `${
-  process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
+  process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : "https://palmwatch-v2.vercel.app"
 }`;
 
 export const metadata = {
-  title: "PalmWatch: Tracking the impact of Big Brands' Palm Oil Use",
+  title: "PalmWatch: Tracking the Impact of Big Brands' Palm Oil Use",
   description:
     "PalmWatch is an innovative open-access platform leveraging advanced data science and open-source intelligence to map the global palm oil supply chain's impact, connecting major brands like Nestlé, PepsiCo, and Unilever to deforestation and environmental changes.",
   keywords:
@@ -21,26 +21,26 @@ export const metadata = {
   metadataBase: new URL(`${VERCEL_DOMAIN}/`),
   openGraph: {
     url: `${VERCEL_DOMAIN}`,
-    title: "PalmWatch:  Tracking the impact of Big Brands' Palm Oil Use",
+    title: "PalmWatch:  Tracking the Impact of Big Brands' Palm Oil Use",
     description:
       "Discover how major brands' palm oil use drives deforestation and environmental change with PalmWatch, an open-access tool powered by data science and open-source intelligence.",
     images: {
       url: `${VERCEL_DOMAIN}/og-image.png`,
       width: "1200",
       height: "630",
-      alt: "PalmWatch: Tracking the impact of Big Brands' Palm Oil Use",
+      alt: "PalmWatch: Tracking the Impact of Big Brands' Palm Oil Use",
     },
   },
   twitter: {
     card: "summary_large_image",
-    title: "PalmWatch: Tracking the impact of Big Brands' Palm Oil Use",
+    title: "PalmWatch: Tracking the Impact of Big Brands' Palm Oil Use",
     description:
       "PalmWatch reveals the environmental footprint of global palm oil consumption by brands like Nestlé, PepsiCo, and Unilever through advanced data analysis.",
     images: {
       url: `${VERCEL_DOMAIN}/og-image.png`,
       width: "1200",
       height: "630",
-      alt: "PalmWatch: Tracking the impact of Big Brands' Palm Oil Use",
+      alt: "PalmWatch: Tracking the Impact of Big Brands' Palm Oil Use",
     },
     site: "@inclusivedevt",
     creator: "@inclusivedevt",
