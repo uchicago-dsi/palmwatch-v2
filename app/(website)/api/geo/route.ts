937af9fb8bfd35317e2c86d778d0a3bd33e15793
nofurtherinformation <dhalpern@uchicago.edu>
2025-07-17 11:58:50 -0500

    Remove unused route

--- app/(website)/api/geo/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export const GET = async (req: NextRequest) => {
-  console.log(req);
-  return NextResponse.json({ message: "Hello, world!" });
-};
