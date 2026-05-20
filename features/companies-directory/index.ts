36d29919a1966a902ff422190b819ed3c440aeef
Tim Hannifan <hannifan@gmail.com>
2026-05-20 07:09:53 -0500

    chore(suppliers): remove legacy company feature paths

--- features/companies-directory/index.ts
+++
@@ -1,4 +0,0 @@
-export type { CompanyEntry } from "./companies-directory-view";
-export { CompaniesClient } from "./companies-directory-view";
-export type { CompanyDirectoryModel } from "./lib/build-company-entries";
-export { buildCompanyDirectoryModel } from "./lib/build-company-entries";
--- features/company-detail/index.ts
+++
@@ -1,2 +0,0 @@
-export type { CompanyPageViewProps } from "./company-page-view";
-export { CompanyPageView } from "./company-page-view";
